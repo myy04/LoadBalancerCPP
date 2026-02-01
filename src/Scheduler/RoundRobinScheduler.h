de1c60c566fdf5034d72f1bdf5013ce030c7dba7
yerdaulet <mussabek.yerdaulet@gmal.com>
2026-02-01 16:49:26 +0800

    made the scheduler contstructor protected

--- src/Scheduler/RoundRobinScheduler.h
+++ src/Scheduler/RoundRobinScheduler.h
@@ -9,6 +9,7 @@ template<typename ServersRequestType, typename ServersResponseType>
 class RoundRobinScheduler: public Scheduler<ServersRequestType, ServersResponseType, RoundRobinScheduler<ServersRequestType, ServersResponseType>> { 
     using ServerType = Server<ServersRequestType, ServersResponseType>;
     using BaseType = Scheduler<ServersRequestType, ServersResponseType, RoundRobinScheduler<ServersRequestType, ServersResponseType>>;
+    friend class LoadBalancer<ServersRequestType, ServersResponseType, RoundRobinScheduler<ServersRequestType, ServersResponseType>>;
 public:
     std::shared_ptr<ServerType> get_server() {
         if (this->servers.empty()) throw std::runtime_error("no servers are associated with this load balancer");
@@ -17,8 +18,8 @@ public:
         if (++current_server_idx >= this->servers.size()) current_server_idx = 0; 
         return server;
     }
-    RoundRobinScheduler(const std::vector<std::shared_ptr<ServerType>>& servers): BaseType(servers), current_server_idx(0) {};
 private:
+    RoundRobinScheduler(const std::vector<std::shared_ptr<ServerType>>& servers): BaseType(servers), current_server_idx(0) {};
     size_t current_server_idx = 0;
 };
 
